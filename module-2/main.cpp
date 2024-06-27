27188487f1a582b8a71abf2d34423c806e33c536
Sirzhatina <mazurkevichsergey@gmail.com>
2024-06-27 03:09:16 +0300

    os and compiler info, alternative way

--- module-2/main.cpp
+++ module-2/main.cpp
@@ -2,10 +2,22 @@
 #include <iostream>
 #include <format>
 
+#include <os_compiler.h>
+
 int main(int argc, char* argv[])
 {
     std::cout 
         << std::format("Factorial of 5 is: {}", factorial(5))
         << std::endl;
+
+    std::cout
+        << std::format("OS: {}\n"
+        "Compiler: {}\n"
+        "Compiler version: {}",
+        OS_NAME, 
+        COMPILER_NAME,
+        COMPILER_VERSION
+    );
+
     return 0;
 }
