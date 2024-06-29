c26238e82fdb5a6df4746cfc5d1293f8a3dcc03d
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-06-29 17:34:16 +0300

    created separate cpp file for system info implementation

--- module-2/system_info.hpp
+++ module-2/system_info.hpp
@@ -1,88 +1,29 @@
 #pragma  once
 
 #include <string>
-#include <format>
 #include <ostream>
 #include <utility>
 
 class SystemInfo {
 public:
-    std::string getOsInfo() const noexcept { return m_osName; }
+    std::string getOsInfo() const noexcept;
 
-    std::string getCompilerVersion() { return m_compilerVersion; }
+    std::string getCompilerVersion();
 
-    static SystemInfo makeSystemInfo() {
-        std::string osName = 
-#ifdef __linux__
-    "Linux"
-#elif _WIN32
-    "Windows"
-#elif __APPLE__
-    "Apple";
-#elif __FreeBSD__
-    "FreeBSD"
-#elif __ANDROID__
-    "Android"
-#else
-    "Unknown"
-#endif 
-        ; std::string compilerInfo = std::format(
-#ifdef __GNUC__
-    "GCC {}.{}.{}", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__
-#elif __clang__
-    "Clang {}.{}.{}", __clang_major__, __clang_minor__, __clang_patchlevel__
-#elif _MSC_VER
-    "MSVC {}", _MSC_VER
-#else
-    "Unknown"
-#endif
-        );
-
-        auto [compilerName, compilerVersion] = parseCompilerInfo(std::move(compilerInfo));
-
-        return SystemInfo{osName, compilerName, compilerVersion};
-    }
+    static SystemInfo makeSystemInfo();
 
     static SystemInfo makeSystemInfoFromCustomSource(
         const char* osName, 
         const char* compilerName, 
         const char* compilerVersion
-    ) {
-        return SystemInfo{osName, compilerName, compilerVersion};
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const SystemInfo& sys) {
-        return os << std::format(
-            "OS name: {}\n"
-            "Compiler name: {}\n"
-            "Compiler version: {}\n",
-            sys.m_osName,
-            sys.m_compilerName,
-            sys.m_compilerVersion
-        ) << std::endl;
-    }
+    );
+    friend std::ostream& operator<<(std::ostream& os, const SystemInfo& sys);
 
 private:
 
-    SystemInfo(std::string osName, std::string compilerName, std::string compilerVersion)
-    : m_osName{std::move(osName)}
-    , m_compilerName{std::move(compilerName)}
-    , m_compilerVersion{std::move(compilerVersion)}
-    {
-        
-    }
+    SystemInfo(std::string osName, std::string compilerName, std::string compilerVersion);
 
-    static std::pair<std::string, std::string> parseCompilerInfo(const std::string compInfo) {
-        auto foundPos = compInfo.find(' ');
-
-        if (foundPos == std::string::npos) {
-            return {compInfo, {}};
-        }
-        
-        return {compInfo.substr(0, foundPos), 
-                compInfo.substr(foundPos)
-        };
-    }
+    static std::pair<std::string, std::string> parseCompilerInfo(const std::string compInfo);
 
 private:
 
