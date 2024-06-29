0fc23b89e4cfeb58fb5d856fda17254066158194
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-06-29 19:16:43 +0300

    factorial argument is taken from command line

--- module-2/main.cpp
+++ module-2/main.cpp
@@ -1,14 +1,28 @@
 #include "factorial.hpp"
+#include <charconv>
+#include <cstdlib>
 #include <iostream>
 #include <format>
 
 #include <os_compiler.h>
+#include <system_error>
 #include "system_info.hpp"
 
 int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        std::cerr << "Usage: program factorial_argument\n";
+        return EXIT_FAILURE;
+    }
+    FACTORIAL_USED_TYPE value{};
+
+    auto [_, ec] = std::from_chars(argv[1], argv[2] - 1, value);
+    if (ec == std::errc::invalid_argument) {
+        std::cerr << "Number given is not valid!";
+        return EXIT_FAILURE;
+    }
     std::cout 
-        << std::format("Factorial of 5 is: {}", factorial(5))
+        << std::format("Factorial of {} is: {}\n", value, factorial(value))
         << std::endl;
 
     std::cout << SystemInfo::makeSystemInfo();
