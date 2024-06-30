d0aa26a48e29bf3eda31d9287af2084585405283
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-06-30 14:27:14 +0300

    split main function content into two separated functions

--- module-2/main.cpp
+++ module-2/main.cpp
@@ -10,18 +10,35 @@
 #include "factorial.hpp"
 #include "system_info.hpp"
 
+void factorialTask(int argc, char* argv[]);
+
+void printSystemInfo(const SystemInfo& sys);
+
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
         std::cerr << "Usage: program factorial_argument\n";
         return EXIT_FAILURE;
     }
+    factorialTask(argc, argv);
+
+    printSystemInfo(SystemInfo::makeSystemInfo());
+    printSystemInfo(SystemInfo{
+        OS_NAME,
+        COMPILER_NAME,
+        COMPILER_VERSION
+    });
+
+    return 0;
+}
+
+void factorialTask(int argc, char* argv[]) {
     FACTORIAL_USED_TYPE value{};
 
     auto [_, ec] = std::from_chars(argv[1], argv[2] - 1, value);
     if (ec == std::errc::invalid_argument) {
         std::cerr << "Number given is not valid!";
-        return EXIT_FAILURE;
+        std::exit(EXIT_FAILURE);
     }
     try {
         std::cout 
@@ -29,16 +46,17 @@ int main(int argc, char* argv[])
             << std::endl;
     } catch (const std::logic_error& le) {
         std::cerr << le.what() << '\n';
-        return EXIT_FAILURE;
+        std::exit(EXIT_FAILURE);
     }
+}
 
-    std::cout << SystemInfo::makeSystemInfo();
-
-    std::cout << SystemInfo::makeSystemInfoFromCustomSource(
-        OS_NAME, 
-        COMPILER_NAME, 
-        COMPILER_VERSION
-    );
-
-    return 0;
+void printSystemInfo(const SystemInfo& sys) {
+    std::cout << std::format(
+        "OS name: {}\n"
+        "Compiler name: {}\n"
+        "Compiler version: {}\n",
+        sys.getOsName(),
+        sys.getCompilerName(),
+        sys.getCompilerVersion()
+    ) << std::endl;
 }
