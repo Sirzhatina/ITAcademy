3abab8c28ece93ef60d2136a9a3001f61a86a8d8
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-06-30 14:52:23 +0300

    SystemInfo class extended with comments

--- module-2/system_info.hpp
+++ module-2/system_info.hpp
@@ -2,6 +2,10 @@
 
 #include <string>
 
+/**
+ * @brief SystemInfo class keeps data like your OS name, compiler name
+ * and compiler version
+ */
 class SystemInfo {
 public:
 
@@ -10,7 +14,12 @@ public:
     std::string getOsName() const noexcept;
     std::string getCompilerName() const noexcept;
     std::string getCompilerVersion() const noexcept;
-
+    /**
+     * @brief Creates new system info instance with data retrieved from the compiler
+     * at preprocessing stage
+     * @throw Same as std::format used internally
+     * @return SystemInfo 
+     */
     static SystemInfo makeSystemInfo();
 
 private:
