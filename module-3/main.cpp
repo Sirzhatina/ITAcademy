0163e49348e5f13a81a5ae14cd75634de707b70b
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-02 20:07:08 +0300

    implemented number parser

--- module-3/main.cpp
+++ module-3/main.cpp
@@ -1,7 +1,9 @@
 #include "table.h"
+#include "number_parser.h"
 
 int main()
 {
     mainTableTask();
+    mainParseNumberTask();
     return 0;
 }
---
+++ module-3/number_parser.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+std::uint32_t input();
+
+std::string parseNumber(std::uint32_t n);
+
+void mainParseNumberTask();
