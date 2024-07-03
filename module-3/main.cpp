d9d2ff65677a13a02748d9e5cdca5afc2ea0c078
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-07-03 10:37:05 +0300

    replaced score variables with objects of class Player

--- module-3/main.cpp
+++ module-3/main.cpp
@@ -11,6 +11,17 @@ void declareDraw() {
     std::cout << "Noone has won. Bye\n";
 }
 
+class Player {
+public:
+    Player() = default;
+
+    std::int16_t getScores() const { return m_scores; }
+    void addScores(std::int16_t scores) { m_scores += scores; }
+
+private:
+    std::int16_t m_scores{0};
+};
+
 int main()
 {
     mainTableTask();
@@ -18,8 +29,8 @@ int main()
 
     std::cout << "\n--------------------------- NOW LET'S PLAY -------------------\n";
 
-    std::int16_t player_1_scores = 0;
-    std::int16_t player_2_scores = 0;
+    Player first{};
+    Player second{};
 
     auto gen = [
         mt = std::mt19937{ std::random_device{}() },
@@ -29,34 +40,34 @@ int main()
     };
 
     for (int i = 0; i < 100; ++i) {
-        player_1_scores += gen();
-        player_2_scores += gen();
+        first .addScores(gen());
+        second.addScores(gen());
 
-        if (player_1_scores >= 50 && player_2_scores >= 50) {
+        if (first.getScores() >= 50 && second.getScores() >= 50) {
             break;
         }
-        else if (player_1_scores >= 50) {
+        else if (first.getScores() >= 50) {
             std::cout << "We've got the winner!" 
-                << std::format(" Player 1 won. Earned scores are: {}", player_1_scores) 
+                << std::format(" Player 1 won. Earned scores are: {}", first.getScores()) 
                 << std::endl;
             return 0;
         }
-        else if (player_2_scores >= 50) {
+        else if (second.getScores() >= 50) {
             std::cout << "We've got the winner!" 
-                << std::format(" Player 2 won. Earned scores are: {}", player_2_scores) 
+                << std::format(" Player 2 won. Earned scores are: {}", second.getScores())
                 << std::endl;
             return 0;
         }
 
-        if (i % 10 == 0) {
+        if (i % 20 == 0) {
             std::cout 
                 << std::format(
                     "Current round is: {}\n"
                     "Player 1 scores: {}\n"
                     "Player 2 scores: {}", 
                     i,
-                    player_1_scores,
-                    player_2_scores
+                    first.getScores(),
+                    second.getScores()
                 ) << std::endl;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
