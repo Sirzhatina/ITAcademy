4a33e99db13729e459cb327fa9d071fe54031fe8
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-07-03 13:01:19 +0300

    a little increasing of readability for the header

--- module-3/number_parser.h
+++ module-3/number_parser.h
@@ -7,6 +7,7 @@
 /// In case the input doesn't meet requirements, the program exits instantly.
 /// @return - the number user entered
 std::uint32_t input();
+
 /// @brief the function parses the number provided as an argument
 /// and returns it as a string with all digits separated by whitespaces
 /// at the same (direct) order
@@ -14,5 +15,6 @@ std::uint32_t input();
 /// @return the string containing the direct-ordered 
 /// number with whitespace-separated digits
 std::string parseNumber(std::uint32_t n);
+
 /// @brief current task's main function to handle the appropriate user output
 void mainParseNumberTask();
