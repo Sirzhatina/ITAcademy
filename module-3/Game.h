8bde8c05263549ffa994c8c119045be675236cc5
Сергей Мазуркевич <mazurkevichsergey@gmail.com>
2024-07-03 12:00:10 +0300

    moved Player class to a separate file

--- module-3/Game.h
+++ module-3/Game.h
@@ -1,27 +1,9 @@
 #pragma once
 
+#include "Player.h"
 #include <cstdint>
 #include <functional>
 
-class Player {
-public:
-    Player()
-    // decided to avoid id 0, let players be counted from 1
-    : m_playerId{++id_counter}
-    {}
-
-    std::int16_t getScores() const { return m_scores; }
-    void addScores(std::int16_t scores) { m_scores += scores; }
-
-    std::size_t getId() const { return m_playerId; }
-
-private:
-    std::int16_t m_scores = 0;
-
-    std::size_t m_playerId;
-
-    static inline std::size_t id_counter = 0;
-};
 
 class Game {
 public:
