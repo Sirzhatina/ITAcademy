8f6d32484782fc4485356257887a73cede105c72
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-10 17:39:17 +0300

    fully implemented number conversion according to the task

--- module-4/main.cpp
+++ module-4/main.cpp
@@ -1,30 +1,11 @@
-#include <concepts>
-#include <format>
-#include <iostream>
-#include <bitset>
-
-constexpr auto bitsInByte = 8;
-auto AsBinary(std::integral auto n) { return std::bitset<sizeof(n) * bitsInByte>{static_cast<std::size_t>(n)}; }
+#include "number_converter.hpp"
 
 int main(int argc, char* argv[])
 {
-    int number = -1;
 
-    std::cout << "Enter the number: ";
-    std::cin >> number;
-    std::cout << std::format(
-        "Binary representation of this number: {}\n",
-            AsBinary(number).to_string()
-        );
-
-    number = (number > 0 && (number & 1) == 0) ? number << 2 : number >> 2;
-
-    std::cout 
-        << std::format(
-            "Shifted number is: {}\n"
-            "Its binary representation is: {}",
-            number, 
-            AsBinary(number).to_string()
-        ) << std::endl;
+    NumberConverter nc{};
+    nc.userInput();
+    nc.printNumber(nc.currentNumber());
+    nc.printNumber(nc.convertValue());
     return 0;
 }
---
+++ module-4/number_converter.cpp
@@ -0,0 +1,41 @@
+#include "number_converter.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <format>
+
+void NumberConverter::userInput() {
+    std::cout << "Enter the number: ";
+    std::cin >> m_userNumber;
+
+    if (!std::cin) {
+        std::cerr << "Input failure.";
+        std::exit(EXIT_FAILURE);
+    }
+
+    m_isSet = true;
+}
+
+bool NumberConverter::isSet() const  { return m_isSet; }
+
+void NumberConverter::reset() { m_isSet = false; }
+
+void NumberConverter::printNumber(int number) const {
+    std::cout << std::format(
+        "The number given: {}"
+        "Binary representation of this number: {}\n",
+        number,
+        AsBinary(number).to_string()
+    );
+}
+
+int NumberConverter::convertValue() const {
+    if (!isSet()) {
+        std::cout << "Warning: the internal value for conversion isn't set." << std::endl;
+    }
+    return (m_userNumber > 0 && (m_userNumber & 1) == 0) ? m_userNumber << 2 : m_userNumber >> 2;
+}
+
+int NumberConverter::currentNumber() const {
+    return m_userNumber;
+}
