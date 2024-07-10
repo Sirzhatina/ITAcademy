d864845372f96b9eb0dc593143d0ef14c1698fa8
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-10 16:57:45 +0300

    first implementration

--- module-4/main.cpp
+++ module-4/main.cpp
@@ -1,5 +1,30 @@
+#include <concepts>
+#include <format>
+#include <iostream>
+#include <bitset>
+
+constexpr auto bitsInByte = 8;
+auto AsBinary(std::integral auto n) { return std::bitset<sizeof(n) * bitsInByte>{static_cast<std::size_t>(n)}; }
+
 int main(int argc, char* argv[])
 {
+    int number = -1;
 
+    std::cout << "Enter the number: ";
+    std::cin >> number;
+    std::cout << std::format(
+        "Binary representation of this number: {}\n",
+            AsBinary(number).to_string()
+        );
+
+    number = (number > 0 && (number & 1) == 0) ? number << 2 : number >> 2;
+
+    std::cout 
+        << std::format(
+            "Shifted number is: {}\n"
+            "Its binary representation is: {}",
+            number, 
+            AsBinary(number).to_string()
+        ) << std::endl;
     return 0;
 }
