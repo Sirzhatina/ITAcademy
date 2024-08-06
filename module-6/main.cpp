247ae73b455e736f643c777f02b6d10966a5161c
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-06 23:17:59 +0300

    moved the first task to a separate main

--- module-6/main.cpp
+++ module-6/main.cpp
@@ -1,8 +1,19 @@
 #include "StringReverser.h"
+#include <iostream>
+
+void mainTask1();
 
 int main(int argc, char* argv[])
 {
-    constexpr auto DEFAULT_STRING_SIZE    = 99;
+    mainTask1();
+    std::cout << std::endl;
+
+    return 0;
+}
+
+
+void mainTask1() {
+    constexpr auto DEFAULT_STRING_SIZE = 99;
     constexpr auto NULL_TERMINATED_STRING_SIZE = DEFAULT_STRING_SIZE + 1;
 
     char infoString[NULL_TERMINATED_STRING_SIZE];
@@ -12,6 +23,4 @@ int main(int argc, char* argv[])
     strRev.takeUserInput(infoString, DEFAULT_STRING_SIZE);
     strRev.reverseString(infoString);
     strRev.printString(infoString);
-    
-    return 0;
 }
