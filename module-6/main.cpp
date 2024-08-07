ce791b2230c7efd954265832373835cf61c709b1
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-07 14:49:34 +0300

    completed the second task

--- module-6/main.cpp
+++ module-6/main.cpp
@@ -1,13 +1,49 @@
 #include "StringReverser.h"
 #include <iostream>
+#include "utility/StreamChecker.h"
+#include <random>
+#include <vector>
+#include <algorithm>
 
 void mainTask1();
+
+int makeValue() {
+    static auto rd = std::random_device{};
+    static auto mt = std::mt19937{ rd() };
+    static auto distr = std::uniform_int_distribution{ 1, 6 };
+
+    return distr(mt);
+}
+
+static StreamChecker strchk{};
+
+void printVector(const std::vector<int>& vec, const std::string& msg) {
+    std::cout << msg;
+    std::ranges::copy(vec, std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << std::endl;
+    strchk.processStreamFailure(std::cout);
+}
+
 
 int main(int argc, char* argv[])
 {
     mainTask1();
     std::cout << std::endl;
 
+    namespace rng = std::ranges;
+
+    std::size_t vectorSize = 0;
+    std::cout << "Input the size of the vector: ";
+    strchk.processStreamFailure(std::cout);
+    std::cin >> vectorSize;
+    strchk.processStreamFailure(std::cin);
+
+    std::vector<int> values(vectorSize);
+    rng::generate(values, makeValue);
+
+    printVector(values, "Vector unsorted: ");
+    rng::sort(values);
+    printVector(values, "Sorted vector: ");
     return 0;
 }
 
