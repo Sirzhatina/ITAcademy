e319e4f5705bc52f3a97ded507f12bb32d147e5d
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-07 13:20:24 +0300

    made adding subdirectory targets more flexible

--- cmake/utility.cmake
+++ cmake/utility.cmake
@@ -5,25 +5,44 @@ function(write_if_not_exist subdir file text)
     endif()
 endfunction()
 
-function(create_main subdir)
-    write_if_not_exist(${subdir} main.cpp
-"int main(int argc, char* argv[])
+function(create_source subdir target_type)
+    if (${target_type} STREQUAL "EXEC" OR ${target_type} STREQUAL "")
+        set(src "main.cpp")
+        set(src_content "int main(int argc, char* argv[])
 \{
 
-    return 0\;
+    return 0\\\;
 \}"
-    )
+        )
+    else()
+        set(src "lib.cpp")
+        set(src_content "#include \"lib.h\"\n\n")
+        # additional header for library source file
+        write_if_not_exist(${subdir} "lib.h" "#pragma once\n\n")
+    endif()
+       
+    write_if_not_exist(${subdir} ${src} ${src_content})
 endfunction()
 
-function(create_cmake_lists subdir)
+function(create_cmake_lists subdir target_type)
+
+    if (${target_type} STREQUAL "EXEC" OR ${target_type} STREQUAL "")
+        set(add_target "add_executable")
+        set(src "main.cpp")
+    else()
+        set(add_target "add_library")
+        set(lib_type "${target_type}")
+        set(src "lib.h lib.cpp")
+    endif()
+
     write_if_not_exist(${subdir} CMakeLists.txt 
 "set(TARGET ${subdir})
 
 set(SOURCES
-    main.cpp
+    ${src}
 )
 
-add_executable($\{TARGET\})
+${add_target}($\{TARGET\} ${lib_type})
 
 target_sources($\{TARGET\}
     PRIVATE
@@ -33,8 +52,18 @@ target_sources($\{TARGET\}
 endfunction()
 
 
-function(add_subdir subdir)
-    create_cmake_lists(${subdir})
-    create_main(${subdir})
+function(add_target_subdir subdir)
+    set(target_type ${ARGN})
+
+    if ("${target_type}" STREQUAL "")
+        set(target_type "EXEC")
+    endif()
+    if (NOT ${target_type} STREQUAL "EXEC" AND NOT ${target_type} STREQUAL "STATIC" AND NOT ${lib_type} STREQUAL "SHARED")
+        message(FATAL_ERROR "-- Fatal, add_target_subdir: passed library type is neither STATIC nor SHARED")
+    endif()
+    
+    create_cmake_lists(${subdir} ${target_type})
+    create_source(${subdir} ${target_type})
+
     add_subdirectory(${subdir})
 endfunction()
