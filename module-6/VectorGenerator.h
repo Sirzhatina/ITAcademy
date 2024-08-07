b2a622039ac14a097e6d223806200709d859d194
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-07 15:35:13 +0300

    second task commented

--- module-6/VectorGenerator.h
+++ module-6/VectorGenerator.h
@@ -5,14 +5,21 @@
 
 #include "utility/StreamChecker.h"
 
+/// @brief class used to internally store a vector with the size passed to the constructor.
+/// Vector also can be sorted and printed.
 class VectorGenerator {
 public:
+    /// @brief constructor used to initally pass the vector size.
+    /// @param vecSize - size that the initialized vector will have
     explicit VectorGenerator(std::size_t vecSize);
-
+    /// @brief method to print to the console the underlying vector values in one line with the whitespace as a delimeter.
+    /// If the internal stream object sets in a fail state during the method execution, 
+    /// the program will be stopped immidiately.
+    /// @param msg - message inserted right before the printed vector.
     void printVector(const std::string& msg);
-
+    /// @brief generates the random values for the underlying vector in the range from 1 to 100.
     void generate();
-
+    /// @brief sorts the underlying vector.
     void sort();
 
 private:
