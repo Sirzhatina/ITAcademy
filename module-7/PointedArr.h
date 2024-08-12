5cd23c3f8d3affe3c07d217187773b17fd315d14
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-12 14:40:35 +0300

    refactored to reduce code duplication

--- module-7/PointedArr.h
+++ module-7/PointedArr.h
@@ -1,23 +1,133 @@
 #pragma once
 
 #include <memory>
+#include <iostream>
+#include <optional>
+#include <numeric>
+#include <algorithm>
+#include "StreamChecker.h"
+#include "utils.h"
 
 namespace hw7 {
 
+    template <template <class> class SmartPtr, class T = int>
+    static constexpr auto isUnique = std::is_same_v<SmartPtr<T[]>, std::unique_ptr<T[]>>;
+
+    template <template <class> class SmartPtr, class T = int>
+    static constexpr auto isWeak = std::is_same_v<SmartPtr<T[]>, std::weak_ptr<T[]>>;
+
+    template <template <class> class SmartPtr, class T = int>
+    static constexpr bool isShared = std::is_same_v<SmartPtr<T[]>, std::shared_ptr<T[]>>;
+
+	template <template <class> class SmartPtr, class T = int>
+    requires
+        isUnique<SmartPtr, T> ||
+        isShared<SmartPtr, T> ||
+        isWeak<SmartPtr, T>
 	class PointedArr {
 	public:
-		PointedArr(std::size_t arrSize);
 
-		[[nodiscard]] std::size_t getSize() const;
+        PointedArr(std::shared_ptr<T[]> sh, std::size_t arrSize) requires isShared<SmartPtr, T> || isWeak<SmartPtr, T>
+            : m_pointer{ sh }
+            , m_arraySize{ arrSize} { }
+        
+		PointedArr(std::size_t arrSize) requires isUnique<SmartPtr, T> || isShared<SmartPtr, T>
+			: m_pointer{ new T[arrSize] }
+			, m_arraySize{ arrSize } { }
 
-		[[nodiscard]] int& operator[](std::size_t index);
-		
-		[[nodiscard]] int operator[](std::size_t index) const;
+		[[nodiscard]]
+		std::size_t getSize() const { return m_arraySize; }
 
-	private:
+		[[nodiscard]]
+		int& operator[](std::size_t index) {
+            if constexpr (isWeak<SmartPtr, T>) {
+                return m_pointer.lock()[index];
+            }
+            else {
+                return m_pointer[index];
+            }
+        }
 
-		std::unique_ptr<int[]> m_pointer;
+		[[nodiscard]]
+		const int& operator[](std::size_t index) const {
+            if constexpr (isWeak<SmartPtr, T>) {
+                return m_pointer.lock()[index];
+            }
+            else {
+                return m_pointer[index];
+            }
+        }
 
-		std::size_t m_arraySize;
+        std::optional<T> sum() const {
+            auto sptr = getSharedOrUnique();
+            
+            if (sptr != nullptr && m_arraySize != 0) {
+                return { std::accumulate(sptr.get(), sptr.get() + m_arraySize, 0) };
+            }
+
+            return {};
+        }
+
+        std::optional<std::pair<int, int>> minmax() const {
+
+            if (auto p = getSharedOrUnique(); p != nullptr && getSize() != 0) {
+                auto res = std::minmax_element(p.get(), p.get() + getSize());
+                return { std::pair{ *res.first, *res.second } };
+            }
+            return {};
+        }
+
+    private:
+        const std::unique_ptr<T[]>& getSharedOrUnique() const requires isUnique<SmartPtr<T[]>> {
+            return m_pointer;
+        }
+        auto getSharedOrUnique() const {
+            std::shared_ptr<T[]> ptr = nullptr;
+
+            if constexpr (isWeak<SmartPtr, T>) {
+                ptr = m_pointer.lock();
+            }
+            else {
+                ptr = m_pointer;
+            }
+
+            return ptr;
+        }
+    private:
+
+        SmartPtr<T[]> m_pointer;
+
+        std::size_t m_arraySize;
+
 	};
+
+    template <template <class> class SPtr, class T = int>
+    void inputElementsOf(hw7::PointedArr<SPtr, T>& pa) {
+        itac::print("Enter {} numbers: ", pa.getSize());
+
+        for (std::size_t i = 0; i < pa.getSize(); ++i) {
+            std::cin >> pa[i];
+        }
+
+        StreamChecker strchk{};
+
+        strchk.processStreamFailure(std::cout);
+        strchk.processStreamFailure(std::cin);
+    }
+
+    template <template <class> class SPtr, class T = int>
+    void printElementsOf(const hw7::PointedArr<SPtr, T>& pa) {
+        itac::print("The numbers you've entered:");
+
+        for (std::size_t i = 0; i < pa.getSize(); ++i) {
+            itac::print(" {}", pa[i]);
+        }
+        std::cout << std::endl;
+
+        StreamChecker strchk{};
+
+        strchk.processStreamFailure(std::cout);
+        strchk.processStreamFailure(std::cin);
+    }
+
 }
--- module-7/main.cpp
+++ module-7/main.cpp
@@ -3,18 +3,7 @@
 #include "utils.h"
 #include "PointedArr.h"
 #include "StreamChecker.h"
-#include <optional>
-#include <algorithm>
-#include <numeric>
 
-void inputElementsOf(std::shared_ptr<int[]> p, std::size_t sz);
-void inputElementsOf(hw7::PointedArr& pa);
-
-void printElementsOf(const std::shared_ptr<int[]> p, std::size_t sz);
-void printElementsOf(const hw7::PointedArr& pa);
-
-std::optional<int> sum(std::weak_ptr<int[]> wp, std::size_t sz);
-std::optional<std::pair<int, int>> minmax(std::weak_ptr<int[]> wp, std::size_t sz);
 
 int main(int argc, char* argv[])
 {
@@ -22,7 +11,7 @@ int main(int argc, char* argv[])
 
     constexpr auto ARRAY_SIZE = 10;
     
-    PointedArr pa{ 10 };
+    PointedArr<std::unique_ptr> pa{ ARRAY_SIZE };
     inputElementsOf(pa);
     printElementsOf(pa);
 
@@ -30,16 +19,17 @@ int main(int argc, char* argv[])
     std::cout << "Enter the size of the shared array: ";
     std::cin >> sharedArraySize;
 
-    auto shp = std::make_shared<int[]>(sharedArraySize);
-    inputElementsOf(shp, sharedArraySize);
+    auto sp = std::make_shared<int[]>(sharedArraySize);
 
-    
-    if (auto s = sum(shp, sharedArraySize); s.has_value()) {
-        itac::print("The sum of all elements is {}", s.value())
+    PointedArr<std::weak_ptr> wp{ sp, sharedArraySize };
+    inputElementsOf(wp);
+
+    if (auto sum = wp.sum(); sum.has_value()) {
+        itac::print("The sum of all elements is {}", sum.value())
             << std::endl;
     }
 
-    if (auto mm = minmax(shp, sharedArraySize); mm.has_value()) {
+    if (auto mm = wp.minmax(); mm.has_value()) {
         itac::print(
             "Min element: {}; max element: {}", 
             mm.value().first, 
@@ -51,64 +41,3 @@ int main(int argc, char* argv[])
 }
 
 
-void inputElementsOf(hw7::PointedArr& pa) {
-    itac::print("Enter {} numbers: ", pa.getSize());
-
-    for (std::size_t i = 0; i < pa.getSize(); ++i) {
-        std::cin >> pa[i];
-    }
-
-    StreamChecker strchk{};
-
-    strchk.processStreamFailure(std::cout);
-    strchk.processStreamFailure(std::cin);
-}
-
-
-void printElementsOf(const hw7::PointedArr& pa) {
-    itac::print("The numbers you've entered:");
-
-    for (std::size_t i = 0; i < pa.getSize(); ++i) {
-        itac::print(" {}", pa[i]);
-    }
-    std::cout << std::endl;
-
-    StreamChecker strchk{};
-
-    strchk.processStreamFailure(std::cout);
-    strchk.processStreamFailure(std::cin);
-}
-
-void inputElementsOf(std::shared_ptr<int[]> p, std::size_t sz) {
-    itac::print("Enter {} numbers: \n", sz);
-
-    for (std::size_t i = 0; i < sz; ++i) {
-        std::cin >> p[i];
-    }
-}
-
-void printElementsOf(const std::shared_ptr<int[]> p, std::size_t sz) {
-    std::cout << "Shared pointed array:";
-    for (std::size_t i = 0; i < sz; ++i) {
-        std::cout << ' ' << p[i];
-    }
-    std::cout << std::endl;
-}
-
-std::optional<int> sum(std::weak_ptr<int[]> wp, std::size_t sz) {
-
-    if (auto p = wp.lock(); p != nullptr && sz != 0) {
-
-        return std::accumulate(p.get(), p.get() + sz, 0);
-    }
-    return {};
-}
-
-std::optional<std::pair<int, int>> minmax(std::weak_ptr<int[]> wp, std::size_t sz) {
-
-    if (auto p = wp.lock(); p != nullptr && sz != 0) {
-        auto res = std::minmax_element(p.get(), p.get() + sz);
-        return { std::pair{ *res.first, *res.second } };
-    }
-    return {};
-}
