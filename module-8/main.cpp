2b73f791284bc3c99e119f4c1df41fd7e5aa3c47
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-13 19:47:35 +0300

    completed the task

--- module-8/main.cpp
+++ module-8/main.cpp
@@ -1,5 +1,36 @@
+#include "Book.h"
+
+#include <iostream>
+#include "utils.h"
+
+
 int main(int argc, char* argv[])
 {
+    std::string author, title;
+    std::uint16_t yearOfPublishing;
+
+    std::cout << "Enter the book data.\nThe author: ";
+    std::getline(std::cin, author);
+
+    std::cout << "The title: ";
+    std::getline(std::cin, title);
+
+    std::cout << "The year of publishing: ";
+    std::cin >> yearOfPublishing;
+
+    hw8::Book b{ author, title, yearOfPublishing };
+
+    b.printInfo();
+
+    std::cout << "\n------\nOr, calling each getter:\n";
+    itac::print(
+        "The author: {}\n"
+        "The title: {}\n"
+        "The year of publishing: {}",
+        b.getAuthor(),
+        b.getTitle(),
+        b.getYearOfPublishing()
+    ) << std::endl;
 
     return 0;
 }
