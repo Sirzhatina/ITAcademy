f6e4e34f52480dccb06f8e60acc08f34bd035673
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-13 19:14:15 +0300

    separeted implementation from the class definition

---
+++ module-8/Book.cpp
@@ -0,0 +1,23 @@
+#include "Book.h"
+
+
+Book::Book(std::string title, std::string author, std::uint16_t yearOfPublishing)
+	: m_title{ std::move(title) }
+	, m_author{ std::move(author) }
+	, m_yearOfPublishing{ yearOfPublishing } { }
+
+const std::string& Book::getTitle() const { return m_title; }
+const std::string& Book::getAuthor() const { return m_author; }
+std::uint16_t Book::getYearOfPublishing() const { return m_yearOfPublishing; }
+
+void Book::printInfo() const {
+	itac::print(
+		"The book info:\n"
+		"The author: {}\n"
+		"The title: {}\n"
+		"The year of publishing: {}",
+		m_author,
+		m_title,
+		m_yearOfPublishing
+	) << std::endl;
+}
--- module-8/Book.h
+++ module-8/Book.h
@@ -6,26 +6,14 @@
 
 class Book {
 public:
-	Book(std::string title, std::string author, std::uint16_t yearOfPublishing)
-		: m_title{std::move(title)}
-		, m_author{std::move(author)}
-		, m_yearOfPublishing{yearOfPublishing} { }
+	Book(std::string title, std::string author, std::uint16_t yearOfPublishing);
 
-	const std::string& getTitle() const { return m_title; }
-	const std::string& getAuthor() const { return m_author;  }
-	std::uint16_t getYearOfPublishing() const { return m_yearOfPublishing; }
+	const std::string& getTitle() const;
+	const std::string& getAuthor() const;
 
-	void printInfo() const {
-		itac::print(
-			"The book info:\n"
-			"The author: {}\n"
-			"The title: {}\n"
-			"The year of publishing: {}",
-			m_author,
-			m_title,
-			m_yearOfPublishing
-		) << std::endl;
-	}
+	std::uint16_t getYearOfPublishing() const;
+
+	void printInfo() const;
 
 private:
 	std::string m_title;
