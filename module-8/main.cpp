c4c825f1974f4317d6a64a6e6243ab2533c4a5d0
Sirzhatina <mazurkevichsergey@gmail.com>
2024-08-13 19:51:18 +0300

    switched the author and the title places

--- module-8/main.cpp
+++ module-8/main.cpp
@@ -9,26 +9,26 @@ int main(int argc, char* argv[])
     std::string author, title;
     std::uint16_t yearOfPublishing;
 
-    std::cout << "Enter the book data.\nThe author: ";
-    std::getline(std::cin, author);
-
-    std::cout << "The title: ";
+    std::cout << "Enter the book data.\nThe title: ";
     std::getline(std::cin, title);
+
+    std::cout << "The author: ";
+    std::getline(std::cin, author);
 
     std::cout << "The year of publishing: ";
     std::cin >> yearOfPublishing;
 
-    hw8::Book b{ author, title, yearOfPublishing };
+    hw8::Book b{ title, author, yearOfPublishing };
 
     b.printInfo();
 
     std::cout << "\n------\nOr, calling each getter:\n";
     itac::print(
-        "The author: {}\n"
         "The title: {}\n"
+        "The author: {}\n"
         "The year of publishing: {}",
-        b.getAuthor(),
         b.getTitle(),
+        b.getAuthor(),
         b.getYearOfPublishing()
     ) << std::endl;
 
