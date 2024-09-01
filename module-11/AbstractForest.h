f73e9cb4738bb30cfe45a98c5f67c70450c1eccd
Sirzhatina <mazurkevichsergey@gmail.com>
2024-09-01 14:27:39 +0300

    documented the task

--- module-11/AbstractForest.h
+++ module-11/AbstractForest.h
@@ -6,22 +6,35 @@
 
 class AbstractTree;
 
+/// @brief class representing the forest that can collect trees, get new trees grown up
+/// inside it and show info about every single tree in the forest.
 class AbstractForest {
 public:
 	AbstractForest() = default;
 	virtual ~AbstractForest() = default;
 
+	/// @brief Method with variadic number of arguments to add every passed tree to the forest
+	/// @tparam ...Tree - A set of parameters of type AbstractTree strictly
+	/// @param ...at - Variadic pointer to trees to add to the forest
+	/// @return - true if all trees have grown up successfully, false otherwise
 	template <std::derived_from<AbstractTree>... Tree>
 	bool growUp(std::shared_ptr<Tree>... at) {
 		return (growUpImpl(at) && ...);
 	}
-
+	/// @brief Prints info about all trees of the forest to the console.
 	void wind() const;
 		
 protected:
+	/// @brief make the passed tree a part of the forest.
+	/// @param at - abstract tree pointer holding actual tree.
+	/// @return treu if tree have been added to the forest, false otherwise.
 	virtual bool growUpImpl(std::shared_ptr<AbstractTree> at) = 0;
 
+	/// @brief const getter for internal tree array.
+	/// @return const reference to the tree array.
 	const auto& trees() const { return m_trees; }
+	/// @brief non-const getter for internal tree array.
+	/// @return non-const reference to the tree array.
 	auto& trees() { return m_trees; }
 
 private:
@@ -30,9 +43,10 @@ private:
 };
 
 
-/// The macro that creates repetitive structure of a class derived from the AbstractForest.
+/// @brief The macro that creates repetitive structure of a class derived from the AbstractForest.
 /// It implements the AbstractForest abstract method and declares only two methods in general, 
 /// one of which is static and must be implemented manually by the user.
+/// @param ForestType - the name of the future forest class.
 #define DefForest(ForestType)										\
 class ForestType : public AbstractForest {							\
 protected:															\
@@ -50,4 +64,4 @@ private:															\
 DefForest(Coniferous)
 DefForest(Deciduous)
 
-#undef DefForest
+#undef DefForest // undefined to prevent misproper usage
--- module-11/AbstractTree.h
+++ module-11/AbstractTree.h
@@ -3,22 +3,36 @@
 #include "TreeKind.h"
 #include "utils.h"
 
+/// @brief Class representing a tree. The actual tree derived from this one
+/// is expected to correspond to one of TreeKind enumeration variants.
 class AbstractTree {
 public:
 	AbstractTree() = default;
 	virtual ~AbstractTree() = default;
 
-	[[nodiscard]]
-	hw9::TreeKind getKind() const;
-
+	/// @brief getter to get underlying TreeKind variant.
+	/// @return the variant of enumeration TreeKind corresponding the actual
+	/// type of tree.
+	[[nodiscard]] hw9::TreeKind getKind() const;
+	/// @brief abstract method which should print the info about the actual tree
+	/// to the console in its own manner.
 	virtual void wind() const = 0;
 
 protected:
+	/// @brief constructs the object from the passed tree kind.
+	/// Should be called only by constructor of derived type that will pass
+	/// the right kind of tree to the parent type.
+	/// @param tk - TreeKind that the tree will have.
 	AbstractTree(hw9::TreeKind tk);
 private:
 	hw9::TreeKind m_kind{ hw9::TreeKind::Unknown };
 };
 
+/// @brief Macro simplifying creation of repetetive types that do similar things.
+/// In this case, macro creates a concrete tree derived from the abstract tree
+/// and overrides its virtual methods. Also, it adds counter that assigns to trees of the current type
+/// as their IDs.
+/// @param TreeName - the name of the future class.
 #define DefTree(TreeName)									   \
 class TreeName : public AbstractTree {						   \
 	static inline std::size_t m_kindCounter = 0;			   \
