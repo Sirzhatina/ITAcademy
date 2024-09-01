7da8badb7ae31df0890726a35b745bec2020e6a5
Sirzhatina <mazurkevichsergey@gmail.com>
2024-09-01 16:24:00 +0300

    documented things

--- module-13/ServiceStation.h
+++ module-13/ServiceStation.h
@@ -7,12 +7,16 @@
 #include <iostream>
 
 namespace hw13 {
+    /// @brief service station class repairing the vehicles. The class expects any type parameter passed
+    /// to be derived from ICar.
     template <std::derived_from<ICar> C>
     class ServiceStation {
         static constexpr std::size_t STATION_CAPACITY = 5;
     public:
+        /// @brief default constructor. Initializes an empty garage for vehicles to repair.
         ServiceStation() = default;
-
+        /// @brief base case for the recursive template method.
+        /// @param car - a car to add to the garage.
         void addCar(std::unique_ptr<C> car) {
             if (m_carsOnRepair.size() >= STATION_CAPACITY) {
                 std::cout << "Sorry, the station is full, come back later\n";
@@ -20,7 +24,11 @@ namespace hw13 {
             }
             m_carsOnRepair.push_back(std::move(car));
         }
-
+        /// @brief method to add cars to the garage to repair them.
+        /// If the garage is full, the rest of the cars won't be added.
+        /// @tparams ...Args - all passed types are same as C which in turn are derived from ICar.
+        /// @param car - fixed argument for base case.
+        /// @param ...cars - cars to be added in a recursive manner.
         template <std::same_as<C>... Args>
         void addCar(std::unique_ptr<C> car, std::unique_ptr<Args>... cars) {
             if (m_carsOnRepair.size() >= STATION_CAPACITY) {
@@ -31,7 +39,9 @@ namespace hw13 {
             std::cout << "Added car successfully!" << std::endl;
             addCar(std::move(cars)...);
         }
-
+        /// @brief method repairs all the cars held in garage and releases them afterwars,
+        /// passing ownership to the caller.
+        /// @return vector of pointers to repaired cars. 
         std::vector<std::unique_ptr<C>> repairAll() {
             std::cout << "All the cars have been repaired\n";
             return std::move(m_carsOnRepair);
