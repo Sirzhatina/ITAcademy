065c6da3d649d899dd4a96d5bc93bc22d86223fb
Sirzhatina <mazurkevichsergey@gmail.com>
2024-09-21 18:45:16 +0300

    created MainWindow

--- module-15/main.cpp
+++ module-15/main.cpp
@@ -1,10 +1,40 @@
 #include <QApplication>
+#include <QMainWindow>
+#include <QScreen>
 
+class MainWindow : public QMainWindow {
+public:
+    MainWindow();
+
+private:
+    QSize getInitialSize();
+
+
+};
 
 int main(int argc, char* argv[])
 {
     QApplication app{ argc, argv };
 
+    MainWindow mw{};
+
+    mw.show();
 
     return app.exec();
+}
+
+
+MainWindow::MainWindow() {
+    setWindowTitle("Currency converter");
+    setFixedSize(getInitialSize());
+}
+
+QSize MainWindow::getInitialSize() {
+    const auto screenSize = this->screen()->size();
+
+    const QSize targetSize{
+        static_cast<int>(screenSize.width() / 2.0),
+        static_cast<int>(screenSize.height() / 2.0)
+    };
+    return targetSize;
 }
